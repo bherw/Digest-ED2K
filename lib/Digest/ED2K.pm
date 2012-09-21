0155378840d5ffdb0cad103691da4126a581756c
Ben Herweyer <benjamin.herweyer@gmail.com>
2012-09-21 19:28:43 +0000

    Perform chunk digest without buffering a full chunk, reset after digest
    Not buffering gives a ~10% speed boost and reduces RAM usage by at least
    9.5 MB.
    Reset after calling digest as per Digest.pm documentation.

--- lib/Digest/ED2K.pm
+++ lib/Digest/ED2K.pm
@@ -1,6 +1,6 @@
 package Digest::ED2K;
 use common::sense;
-use Digest::MD4;
+use Digest::MD4 ();
 use Exporter 'import';
 our @EXPORT_OK = qw(ed2k ed2k_hex ed2k_base64);
 use version 0.77; our $VERSION = version->declare('v1.0');
@@ -12,51 +12,53 @@ use constant CHUNK_SIZE => 9728000;
 
 sub new {
 	my $class = shift;
-	bless {
-		ctx => Digest::MD4->new,
-		blocks => 0,
-		buffer => '',
-		_digest => undef,
-	}, ref($class) || $class;
+	bless {}, ref $class || $class;
 }
 
 sub clone {
 	my $self = shift;
 	bless {
-		ctx => $self->{ctx}->clone,
-		blocks => $self->{blocks},
-		buffer => $self->{buffer},
-		_digest => $self->{_digest},
+		($self->{ctx} ? (ctx => $self->{ctx}->clone) : ()),
+		($self->{chunk_ctx} ? (chunk_ctx => $self->{chunk_ctx}->clone) : ()),
+		chunk_length => $self->{chunk_length},
 	}, ref($self);
 }
 
 sub add {
 	my $self = shift;
-	if(defined $self->{_digest}) {
-		require Carp;
-		Carp::croak("Can't add to a ed2k digest after it's been finalized. Please reset the object if you wish to reuse it.");
+	my $buffer = join '', @_;
+
+	while ($buffer) {
+		my $need_length = CHUNK_SIZE - $self->{chunk_length};
+		my $chunk_ctx = $self->{chunk_ctx} ||= Digest::MD4->new;
+
+		my $substr = substr $buffer, 0, $need_length;
+		$chunk_ctx->add($substr);
+		$self->{chunk_length} += length $substr;
+
+		if ($self->{chunk_length} == CHUNK_SIZE) {
+			my $ctx = $self->{ctx} ||= Digest::MD4->new;
+
+			$ctx->add( $chunk_ctx->digest );
+			$self->{chunk_length} = 0;
+		}
+
+		return $self if $need_length >= length $buffer;
+		$buffer = substr $buffer, $need_length;
 	}
-	$self->{buffer} .= join '', @_;
-	while(length($self->{buffer}) >= CHUNK_SIZE) {
-		$self->{ctx}->add(Digest::MD4->new->add(substr($self->{buffer}, 0, CHUNK_SIZE))->digest);
-		$self->{buffer} = substr($self->{buffer}, CHUNK_SIZE);
-		$self->{blocks}++;
-	}
-	$self;
+
+	return $self;
 }
 
 sub digest {
 	my $self = shift;
-	return $self->{_digest} if defined $self->{_digest};
-	if(!$self->{blocks}) {
-		$self->{_digest} = Digest::MD4->new->add($self->{buffer})->digest;
-	}
-	else {
-		$self->{ctx}->add(Digest::MD4->new->add($self->{buffer})->digest);
-		$self->{buffer} = '';
-		$self->{_digest} = $self->{ctx}->digest;
-	}
-	return $self->{_digest};
+	my ($ctx, $chunk_ctx) = delete @$self{qw( ctx chunk_ctx chunk_length )};
+
+	# One chunk
+	return $chunk_ctx->digest unless $ctx;
+
+	# Multi chunk
+	$ctx->add( $chunk_ctx->digest )->digest;
 }
 
 sub ed2k(@) {
